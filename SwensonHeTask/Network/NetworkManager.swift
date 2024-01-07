72fbbb7f86757676727e4dcfdf5e8bf1a92880bb
Mostafa Alaa <mostafaassassin@gmail.com>
2024-01-07 22:17:14 +0200

    added url session caching

--- SwensonHeTask/Network/NetworkManager.swift
+++ SwensonHeTask/Network/NetworkManager.swift
@@ -10,8 +10,14 @@ import Foundation
 struct NetworkManager {
     static let shared = NetworkManager()
 
-    private init() {}
-
+    private init() {
+        configureURLSession()
+    }
+    private func configureURLSession() {
+        let cache = URLCache(memoryCapacity: 10 * 1024 * 1024, diskCapacity: 50 * 1024 * 1024, diskPath: nil)
+        URLSession.shared.configuration.urlCache = cache
+        URLSession.shared.configuration.requestCachePolicy = .reloadRevalidatingCacheData
+    }
     func request<T: Decodable>(_ url: URL) async throws -> T {
         guard let data = try? await URLSession.shared.data(from: url).0 else {
             throw NetworkError.requestFailed
